fbfa15caca830971c5d7081768f1c27a7f835fbd
zhang-shengping <zhangshengping2012@hotmail.com>
2019-12-03 19:00:23 -0800

    add http and dns test

--- test_network/dns-test.py
+++ test_network/dns-test.py
@@ -1,5 +1,35 @@
-from scapy.all import *
+# import eventlet
+# eventlet.monkey_patch()
 
-# answer = sr1(IP(src="30.50.215.62",dst="10.10.1.37")/UDP(sport=RandShort(),dport=53)/DNS(rd=1,qd=DNSQR(qname="nginx-website.com")),verbose=0)
-answer = sr1(IP(src="30.50.215.62",dst="10.10.1.37")/UDP(sport=RandShort(),dport=53)/DNS(rd=1,qd=DNSQR(qname="website.com")),verbose=0)
-print answer[DNS].summary()
+from geoip import geolite2
+from scapy.all import *
+import string
+import random
+
+
+QUERY_NAMES = [
+  "google.com",
+  "www.website.com",
+  "baidu.com",
+  "onion.com",
+  "website.com",
+]
+
+def random_query_name():
+  letters = string.ascii_letters
+  length = random.randint(1,254)
+  rand_source = ''.join(random.choice(letters) for i in range(length))
+  QUERY_NAMES.append(rand_source)
+  return random.choice(QUERY_NAMES)
+
+
+def random_sources():
+  SOURCE = ['.'.join((str(random.randint(1,254)) for _ in range(4))) for _ in range(100)]
+  return [ip for ip in SOURCE if geolite2.lookup(ip)]
+  
+
+for source_ip in random_sources():
+  query_name = random_query_name()
+  packet = IP(src=source_ip, dst="10.10.1.37")/UDP(sport=RandShort(),dport=53)/DNS(rd=1,qd=DNSQR(qname=query_name))
+  answer = sr1(packet)
+  print answer[DNS].summary()
--- test_network/http-test.py
+++ test_network/http-test.py
@@ -3,6 +3,10 @@ eventlet.monkey_patch()
 
 import requests
 import random
+import datetime
+import string
+from geoip import geolite2
+
 
 USER_AGENTS = ( 
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_7_0; en-US) AppleWebKit/534.21 (KHTML, like Gecko) Chrome/11.0.678.0 Safari/534.21",
@@ -12,26 +16,105 @@ USER_AGENTS = (
     "Opera/9.80 (X11; U; Linux i686; en-US; rv:1.9.2.3) Presto/2.2.15 Version/10.10"
 )
 
+NORMAL_URIS = [
+    "/",
+    "/user",
+    "/user/authorize",
+    "/user/login",
+    "/user/create",
+    "/user/delete",
+    "/test/delete",
+    "/list/prices.php",
+    "/list/users.php",
+    "/list/photographer.php",
+    "/list/paintings.php",
+    "/download/products/demo.jpeg",
+    "/download/info/demo.ico",
+    "/download/ticket/demo.png",
+]
+
+NGINX_URIS = [
+    "/nginx_plusgins/giom.js",
+    "/nginx_plusgins/rsms.js",
+    "/nginx_plusgins/kaltura.js",
+    "/nginx_plusgins/openresty.js",
+    "/nginx_plusgins/atomx.js",
+    "/nginx_plusgins/nginx-clojure.js",
+    "/nginx_plusgins/openresty.js"
+]
+
+VIRUS_NAMES = (
+    "macro",
+    "script"
+)
+
+VIRUS_SUFFIXS = (
+    "exe",
+    "c",
+    "py"
+)
+
+USERS = (
+    "Bob",
+    "Alice",
+    "Tom",
+    "Jerry",
+    "Amber",
+    "Will",
+)
+
+def random_query_name(num):
+  letters = string.ascii_letters
+  length = num
+  rand_source = ''.join(random.choice(letters) for i in range(length))
+  return rand_source
+
+ACTIONS = (
+    "/search/thing?id=",
+    "/create/thing?id=",
+    "/add/thing?id=",
+    "/update/thing?id=",
+    "/delete/thing?id="
+)
+ACTIONS_URIS = [random.choice(ACTIONS) + random_query_name(10)   for i in range(50)]
+
 DOMAIN = "http://nginx-website.com"
+# DOMAIN = "http://ng1.com"
 SOURCE = ['.'.join((str(random.randint(1,254)) for _ in range(4))) for _ in range(100)]
+GEO_SOURCE = [ip for ip in SOURCE if geolite2.lookup(ip)]
 
 def fetch(num):
+    ALL_URIS = []
+    rand_virus = '/' + random.choice(VIRUS_NAMES) + '/' + random_query_name(6) + '.' + random.choice(VIRUS_SUFFIXS)
+    user = random.choice(USERS)
+    ALL_URIS = NORMAL_URIS + ACTIONS_URIS + NGINX_URIS + [rand_virus]
+    domain = DOMAIN + random.choice(ALL_URIS) 
     spoof_src = random.choice(SOURCE)
     user_agent = random.choice(USER_AGENTS)
-    headers = {'X-Forwarded-For':spoof_src, 'User-Agent':user_agent}
+    cookie = random_query_name(15)
+    headers = {
+               'X-Forwarded-For':spoof_src, 
+               'User-Agent':user_agent,
+               'Cookie':cookie,
+              }
     
     # (NOTE) pzhang: test for event sleep in coroutines
     # if num == 1:
          # headers = {'X-Forwarded-For':"test", 'User-Agent': "test"}
          # eventlet.sleep(20)
-    r = requests.get(DOMAIN, headers = headers)
-    # print r
+    print "domain is: " + domain
+    methods = [requests.get, requests.patch, requests.put, requests.post, requests.delete]  
+    request_action = random.choice(methods)
+    # r = requests.get(domain, headers=headers, auth=(user,'pass'), timeout=20)
+    r = request_action(domain, headers=headers, auth=(user,'pass'), timeout=20)
+    print r
     return r
 
-pool = eventlet.GreenPool(10)
-for r in range(100): 
+pool = eventlet.GreenPool(1000)
+for r in range(600): 
     pool.spawn(fetch, r)
     # (NOTE) pzhang: this will block everything, since it is not in corountine
     # if r == 1:
     #     eventlet.sleep(20)
 pool.waitall()
+print "finished %s"%datetime.datetime.now()
