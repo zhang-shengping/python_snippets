a795c93faccd80dbe85f6846bd5285178713b217
pzhang <peter.zhang@f5.com>
2022-03-08 08:32:31 +0000

    bigip test

--- test_bigip_icontrol/route.py
+++ test_bigip_icontrol/route.py
@@ -7,12 +7,31 @@ from f5_openstack_agent.lbaasv2.drivers.bigip.resource_helper import \
 from f5.bigip import ManagementRoot
 
 bigip = ManagementRoot(
-    "10.250.2.211",
+    # "10.250.2.211",
+    # "admin",
+    # "P@ssw0rd123",
+    "10.145.70.136",
     "admin",
-    "P@ssw0rd123"
+    "Passw0rd@F5",
     timeout=10,
     debug=True
 )
-self.route_manager = BigIPResourceHelper(ResourceType.route)
+route_manager = BigIPResourceHelper(ResourceType.route)
+member_manager = BigIPResourceHelper(ResourceType.member)
+# route_manager.load(bigip, name="xxx", partition="Common")
+result = route_manager.get_resources(bigip, partition="Common")
+print result
 
-self.route_manager.get
+result = member_manager.get_resources(bigip, partition="Common")
+print result
+
+# model={
+    # "name":"xxxx",
+    # "partition": "Common",
+    # "network":"10.250.34.0%56/24"
+    # "pool":"/Common/Default_GW_Pool"
+# }
+# result = route_manager.create(bigip, model)
+# print result
+
+
